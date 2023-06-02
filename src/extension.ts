a0826e5e01837ba694a54d54bb9bb097432b430f
Chaojian Zhang <szinubuntu@gmail.com>
2023-06-02 11:33:41 -0400

    [Sort Line Items (CSV)] Preliminary setup

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// The extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('MyComands extension is activated!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mycommands" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
+	// Provide implementations to the comamnds defined in the package.json file;
+	// Commands defined in package.json file do not require extention to be loaded to display.
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mycommands.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MyCommands!');
+	let disposable = vscode.commands.registerCommand('mycommands.sortline_csv', () => {
+		let activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			return;
+		}
+		
+		let text = activeEditor.document.getText();
+		console.log(text);
+
+		vscode.window.showInformationMessage('Done!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
